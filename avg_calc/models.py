73b29de487c659944d7643263182cf965cc80492
ashish <ashish.kekaan@lemonlabs.in>
2024-08-07 15:43:41 +0530

    Refactor code in model, form and views

--- avg_calc/models.py
+++ avg_calc/models.py
@@ -4,14 +4,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class SalaryExpenses(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.user.username
-
-
 class WorkTimeEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
@@ -44,6 +36,14 @@ class DailyWorkSummary(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.date}"
+
+
+class SalaryExpenses(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Leave(models.Model):
