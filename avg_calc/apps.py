7d559b4741101f9e7450351492b8aeaf410dccd3
ashish <ashish.kekaan@lemonlabs.in>
2024-08-07 18:14:08 +0530

    Some changes in code

--- avg_calc/apps.py
+++ avg_calc/apps.py
@@ -1,6 +1,13 @@
+"""
+Django application configuration for the AvgCalc app.
+"""
+
 from django.apps import AppConfig
 
 
 class AvgCalcConfig(AppConfig):
+    """
+    Configuration for the AvgCalc application.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "avg_calc"
