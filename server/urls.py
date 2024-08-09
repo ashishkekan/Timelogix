dc12cd8aa37fda5b3d3237820a7ec96e33956d44
ashish <ashish.kekaan@lemonlabs.in>
2024-08-09 12:39:18 +0530

    Design recent activity and create their view and template

--- server/urls.py
+++ server/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('apply-leave/', views.add_leave, name='apply-leave'),
     path('create_task/', views.create_task, name='create_task'),
     path('task_list/', views.task_list, name='task_list'),
+    path('recent_activity/', views.recent_activity, name='recent_activity'),
     path('', include('django.contrib.auth.urls')),
 ]
 
