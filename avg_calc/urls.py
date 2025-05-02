d851f9651e41e8d73e46a64a8a8349cb3adf3c77
ashish <ashish.kekaan@lemonlabs.in>
2025-05-02 14:25:32 +0530

    Set proper urls

--- avg_calc/urls.py
+++ avg_calc/urls.py
@@ -1,3 +1,5 @@
+# avg_calc/urls.py
+
 from django.urls import path
 
 from . import views
@@ -5,4 +7,25 @@ from . import views
 urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile_view, name="profile"),
+    path("worktime/", views.work_view, name="worktime"),
+    path("submit/", views.submit_work_time, name="submit_work_time"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("upload_time_logs/", views.upload_time_logs, name="upload_time_logs"),
+    path("download_template/", views.download_template, name="download_template"),
+    path("expenses/", views.total_expenses, name="expenses"),
+    path("update-salary/", views.update_salary_expenses, name="update_salary_expenses"),
+    path(
+        "salary-success/", views.salary_expenses_success, name="salary_expenses_success"
+    ),
+    path("calculate-work-time/", views.calculate_work_time, name="calculate_work_time"),
+    path(
+        "work-summary/<int:pk>/", views.work_summary_detail, name="work_summary_detail"
+    ),
+    path("leaves/", views.leaves, name="leaves"),
+    path("apply-leave/", views.add_leave, name="apply-leave"),
+    path("create_task/", views.create_task, name="create_task"),
+    path("task_list/", views.task_list, name="task_list"),
+    path("recent_activity/", views.recent_activity, name="recent_activity"),
 ]
