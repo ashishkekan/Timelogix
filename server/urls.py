16c7dee6c7bba70bdbc9be345d03fc3789a2004a
ashish <ashish.kekaan@lemonlabs.in>
2025-05-02 13:59:21 +0530

    Fixed layouts and added proper urls

--- server/urls.py
+++ server/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("worktime/", include("avg_calc.urls")),
     path("profile/", views.profile_view, name="profile"),
     path("submit/", views.submit_work_time, name="submit_work_time"),
+    path("timelogs/", views.work_view, name="timelogs"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("upload_time_logs/", views.upload_time_logs, name="upload_time_logs"),
     path("download_template/", views.download_template, name="download_template"),
@@ -38,6 +39,7 @@ urlpatterns = [
     path(
         "work-summary/<int:pk>/", views.work_summary_detail, name="work_summary_detail"
     ),
+    path("leaves/", views.leaves, name="leaves"),
     path("apply-leave/", views.add_leave, name="apply-leave"),
     path("create_task/", views.create_task, name="create_task"),
     path("task_list/", views.task_list, name="task_list"),
