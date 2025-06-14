022f3dd9d7c289dce74c837836f92bc919266b8f
ashish <ashish.kekaan@lemonlabs.in>
2025-06-14 13:37:17 +0530

    revert

--- avg_calc/views.py
+++ avg_calc/views.py
@@ -702,7 +702,7 @@ def leaves(request):
 @login_required
 def users(request):
     users = User.objects.all()
-    paginator = Paginator(users, 1)
+    paginator = Paginator(users, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
